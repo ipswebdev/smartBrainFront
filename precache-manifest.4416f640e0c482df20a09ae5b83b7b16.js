91c9a73f8242249527d311fc01db4aa13badb48e
ipswebdev <sawant.pranay.15ee1102@gmail.com>
2019-07-30 20:35:59 +0530

    Updates

---
+++ precache-manifest.4416f640e0c482df20a09ae5b83b7b16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6910ff0884ade1d149a1976209f98bbb",
+    "url": "/smartBrainFront/index.html"
+  },
+  {
+    "revision": "86ba1a9d2b5b1bc023df",
+    "url": "/smartBrainFront/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "dbf669675e6b53bf38cc",
+    "url": "/smartBrainFront/static/css/main.f91238fa.chunk.css"
+  },
+  {
+    "revision": "86ba1a9d2b5b1bc023df",
+    "url": "/smartBrainFront/static/js/2.7ca30bdf.chunk.js"
+  },
+  {
+    "revision": "dbf669675e6b53bf38cc",
+    "url": "/smartBrainFront/static/js/main.5263537d.chunk.js"
+  },
+  {
+    "revision": "f814ecbd6bce8b090140",
+    "url": "/smartBrainFront/static/js/runtime~main.d8273546.js"
+  }
+]);
